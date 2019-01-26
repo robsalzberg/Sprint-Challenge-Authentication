a2760c6900235f2d20239350be999343572c1a9c
Rob Salzberg <robsalzberg@gmail.com>
2019-01-26 13:08:48 -0500

    Implemented the `register` function

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,47 @@
-const axios = require('axios');
-
-const { authenticate } = require('../auth/authenticate');
+const axios = require("axios");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
+const jwtKey = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const secret = jwtKey;
+  const options = {
+    expiresIn: "1h",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  if (!creds.username || !creds.password) {
+    const errorMessage = "Please provide both a username and password";
+    res.status(400).json({ errorMessage });
+    return;
+  }
+  const hash = bcrypt.hashSync(creds.password, 4);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
 }
 
 function login(req, res) {
@@ -18,15 +50,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
